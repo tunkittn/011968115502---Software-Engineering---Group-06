135b9f0f3ba03ab980043e87b0e4e2d10f1363cf
tunkittn <ngkit09155@gmail.com>
2026-04-26 10:28:25 +0700

    upd

--- 011968115502---Software-Engineering---Group-06/database.py
+++ 011968115502---Software-Engineering---Group-06/database.py
@@ -1,1 +1,121 @@
+import sqlite3
 
+DB_NAME = "phonebook.db"
+
+
+def connect_db():
+    return sqlite3.connect(DB_NAME)
+
+
+def init_database():
+    conn = connect_db()
+    cursor = conn.cursor()
+
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS groups (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL UNIQUE
+    )
+    """)
+
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS contacts (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        phone TEXT NOT NULL,
+        email TEXT,
+        address TEXT,
+        group_id INTEGER,
+        FOREIGN KEY (group_id) REFERENCES groups(id)
+    )
+    """)
+
+    conn.commit()
+    conn.close()
+
+
+
+def insert_contact(name, phone, email=None, address=None, group_id=None):
+    conn = connect_db()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    INSERT INTO contacts (name, phone, email, address, group_id)
+    VALUES (?, ?, ?, ?, ?)
+    """, (name, phone, email, address, group_id))
+
+    conn.commit()
+    conn.close()
+
+
+def get_all_contacts():
+    conn = connect_db()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    SELECT contacts.id, contacts.name, contacts.phone,
+           contacts.email, contacts.address, groups.name
+    FROM contacts
+    LEFT JOIN groups ON contacts.group_id = groups.id
+    ORDER BY contacts.name ASC
+    """)
+
+    data = cursor.fetchall()
+    conn.close()
+    return data
+
+
+def update_contact(contact_id, name, phone, email, address, group_id):
+    conn = connect_db()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    UPDATE contacts
+    SET name=?, phone=?, email=?, address=?, group_id=?
+    WHERE id=?
+    """, (name, phone, email, address, group_id, contact_id))
+
+    conn.commit()
+    conn.close()
+
+
+def delete_contact(contact_id):
+    conn = connect_db()
+    cursor = conn.cursor()
+
+    cursor.execute("DELETE FROM contacts WHERE id=?", (contact_id,))
+
+    conn.commit()
+    conn.close()
+
+
+def search_contacts(keyword):
+    conn = connect_db()
+    cursor = conn.cursor()
+
+    query = f"%{keyword}%"
+
+    cursor.execute("""
+    SELECT contacts.id, contacts.name, contacts.phone,
+           contacts.email, contacts.address, groups.name
+    FROM contacts
+    LEFT JOIN groups ON contacts.group_id = groups.id
+    WHERE contacts.name LIKE ? OR contacts.phone LIKE ?
+    """, (query, query))
+
+    data = cursor.fetchall()
+    conn.close()
+    return data
+
+
+def get_all_groups():
+    conn = connect_db()
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM groups")
+    data = cursor.fetchall()
+
+    conn.close()
+    return data
--- 011968115502---Software-Engineering---Group-06/group_manager.py
+++ 011968115502---Software-Engineering---Group-06/group_manager.py
@@ -1,1 +1,83 @@
+from database import connect_db
 
+
+def create_group(group_name):
+    conn = connect_db()
+    cursor = conn.cursor()
+
+    try:
+        cursor.execute("INSERT INTO groups (name) VALUES (?)", (group_name,))
+        conn.commit()
+        print("Group created successfully!")
+    except:
+        print("Group already exists!")
+
+    conn.close()
+
+
+def delete_group(group_id):
+    conn = connect_db()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    UPDATE contacts
+    SET group_id = NULL
+    WHERE group_id = ?
+    """, (group_id,))
+
+    cursor.execute("DELETE FROM groups WHERE id=?", (group_id,))
+
+    conn.commit()
+    conn.close()
+    print("Group deleted successfully!")
+
+
+def rename_group(group_id, new_name):
+    conn = connect_db()
+    cursor = conn.cursor()
+
+    try:
+        cursor.execute("""
+        UPDATE groups
+        SET name=?
+        WHERE id=?
+        """, (new_name, group_id))
+
+        conn.commit()
+        print("Group renamed successfully!")
+    except:
+        print("Error renaming group!")
+
+    conn.close()
+
+
+def assign_contact_to_group(contact_id, group_id):
+    conn = connect_db()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    UPDATE contacts
+    SET group_id=?
+    WHERE id=?
+    """, (group_id, contact_id))
+
+    conn.commit()
+    conn.close()
+
+    print("Contact assigned to group!")
+
+
+def get_contacts_by_group(group_id):
+    conn = connect_db()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    SELECT id, name, phone, email, address
+    FROM contacts
+    WHERE group_id=?
+    """, (group_id,))
+
+    data = cursor.fetchall()
+    conn.close()
+
+    return data
