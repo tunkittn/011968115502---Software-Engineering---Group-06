87fab540ccf7ccf8eb144780f26d482e29ae1199
huybui0206 <buigiahuy0206@gmail.com>
2026-04-30 15:40:22 +0700

    update

--- 011968115502---Software-Engineering---Group-06/delete_contact.py
+++ 011968115502---Software-Engineering---Group-06/delete_contact.py
@@ -1,1 +1,15 @@
+from database import load_contacts, save_contacts
 
+def delete_contact():
+    contacts = load_contacts()
+
+    name = input("Enter contact name to delete: ")
+
+    for contact in contacts:
+        if contact["name"].lower() == name.lower():
+            contacts.remove(contact)
+            save_contacts(contacts)
+            print("Deleted successfully!")
+            return
+
+    print("Contact not found!")
