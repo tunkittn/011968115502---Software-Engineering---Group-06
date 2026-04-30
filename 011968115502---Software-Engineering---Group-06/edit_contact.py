7dc131abf139dd94313d7988af2d93bbee74c410
huybui0206 <buigiahuy0206@gmail.com>
2026-04-30 15:34:43 +0700

    update

--- 011968115502---Software-Engineering---Group-06/edit_contact.py
+++ 011968115502---Software-Engineering---Group-06/edit_contact.py
@@ -1,1 +1,21 @@
+from database import load_contacts, save_contacts
 
+def edit_contact():
+    contacts = load_contacts()
+
+    name = input("Enter contact name to edit: ")
+
+    for contact in contacts:
+        if contact["name"].lower() == name.lower():
+            print("Old Information:")
+            print("Name:", contact["name"])
+            print("Phone:", contact["phone"])
+
+            new_phone = input("Enter new phone number: ")
+            contact["phone"] = new_phone
+
+            save_contacts(contacts)
+            print("Updated successfully!")
+            return
+
+    print("Contact not found!")
--- 011968115502---Software-Engineering---Group-06/main.py
+++ 011968115502---Software-Engineering---Group-06/main.py
@@ -1,1 +1,13 @@
+n=int(input("Enter the number of elements in the array: "))
+arr=list(map(int,input("Enter the elements of the array: ").split()))
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+sorted_arr = bubble_sort(arr)
+print("Sorted array is:", sorted_arr)
+    
 
